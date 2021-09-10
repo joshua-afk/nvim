f5a6a9e1bd58120f6a3dd024b24d0fb0a947d472
Joshua Ryan V. Velasquez <joshuaryan.velasquez.0322@gmail.com>
2021-09-10 22:49:10 +0800

    feat: add Packer package manager for lua plugins

--- lua/init.lua
+++ lua/init.lua
@@ -1,6 +1,36 @@
+local vim = vim
+local execute = vim.api.nvim_command
+local fn = vim.fn
+
+vim.cmd('packadd packer.nvim')
+local packer = require'packer'
+local util = require'packer.util'
+packer.init({
+    package_root = util.join_paths(vim.fn.stdpath('data'), 'site', 'pack')
+})
+--- startup and add configure plugins
+packer.startup(function()
+    local use = use
+
+    use 'wbthomason/packer.nvim'
+    use 'lewis6991/impatient.nvim'
+
+    -- Linters & Fixers
+    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+
+    -- LSP, Completions & Snippets
+    use 'neovim/nvim-lspconfig'
+    use {
+        "hrsh7th/nvim-cmp",
+        requires = {
+            "hrsh7th/cmp-buffer",
+        }
+    }
+end
+)
+
+-- Load configurations
 local init = function ()
     require("_plugins-config/_cmp")
 end
-
 init() --> Load configs
-
