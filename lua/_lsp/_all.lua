afa22b1b957b45475f7880a057ef984a580ee554
Joshua Ryan V. Velasquez <joshuaryan.velasquez.0322@gmail.com>
2021-09-12 14:08:49 +0800

    refactor: add lsp config file

---
+++ lua/_lsp/_all.lua
@@ -0,0 +1,60 @@
+--Enable (broadcasting) snippet capability for completion
+-- local capabilities = vim.lsp.protocol.make_client_capabilities()
+-- capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+-- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
+-- HTML
+require'lspconfig'.html.setup {
+  cmd = { "vscode-html-language-server", "--stdio" },
+    filetypes = { "html" },
+    init_options = {
+      configurationSection = { "html", "css", "javascript" },
+      embeddedLanguages = {
+        css = true,
+        javascript = true
+      },
+    },
+    root_dir = function(fname)
+          return util.root_pattern('package.json', '.git')(fname) or util.path.dirname(fname)
+        end,
+    settings = {},
+  capabilities = capabilities,
+  on_attach=require'cmp_nvim_lsp'.on_attach,
+}
+
+-- PHP
+require'lspconfig'.intelephense.setup{
+  on_attach=require'cmp_nvim_lsp'.on_attach,
+  capabilities = capabilities,
+}
+
+-- Python
+require'lspconfig'.pyright.setup{
+    cmd = { "pyright-langserver", "--stdio" },
+    filetypes = { "python" },
+    root_dir = function(fname)
+        local root_files = {
+            'pyproject.toml',
+            'setup.py',
+            'setup.cfg',
+            'requirements.txt',
+            'Pipfile',
+            'pyrightconfig.json',
+        }
+        return util.root_pattern(unpack(root_files))(fname) or util.find_git_ancestor(fname) or util.path.dirname(fname)
+    end,
+    settings = {
+        python = {
+            analysis = {
+                autoSearchPaths = true,
+                diagnosticMode = "workspace",
+                useLibraryCodeForTypes = true
+            }
+        }
+    },
+    on_attach=require'cmp_nvim_lsp'.on_attach,
+    capabilities = capabilities,
+}
