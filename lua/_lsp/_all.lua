a2fffd787311c75320dfd7fcb9e93b6f8ce1c432
Joshua Ryan V. Velasquez <joshuaryan.velasquez.0322@gmail.com>
2021-10-23 21:53:44 +0800

    feat: add eslint at lsp

--- lua/_lsp/_all.lua
+++ lua/_lsp/_all.lua
@@ -7,55 +7,106 @@ local coq = require"coq"
 
 -- HTML
 lsp.html.setup {
-    cmd = { "vscode-html-language-server", "--stdio" },
-    filetypes = { "html" },
-    init_options = {
-        configurationSection = { "html", "css", "javascript" },
-        embeddedLanguages = {
-            css = true,
-            javascript = true
-        },
+  cmd = { "vscode-html-language-server", "--stdio" },
+  filetypes = { "html" },
+  init_options = {
+    configurationSection = { "html", "css", "javascript" },
+    embeddedLanguages = {
+      css = true,
+      javascript = true
     },
-    root_dir = function(fname)
-        return util.root_pattern('package.json', '.git')(fname) or util.path.dirname(fname)
-    end,
-    settings = {},
-    capabilities = capabilities,
+  },
+  root_dir = function(fname)
+    return util.root_pattern('package.json', '.git')(fname) or util.path.dirname(fname)
+  end,
+  settings = {},
+  capabilities = capabilities,
 }
 
--- PHP
-lsp.intelephense.setup {
-    coq.lsp_ensure_capabilities{
-        cmd = { "intelephense", "--stdio" },
-        filetypes = { "php" },
-        capabilities = capabilities,
-    }
-}
-
--- Python
-lsp.pyright.setup{
-    cmd = { "pyright-langserver", "--stdio" },
-    filetypes = { "python" },
-    root_dir = function(fname)
-        local root_files = {
-            'pyproject.toml',
-            'setup.py',
-            'setup.cfg',
-            'requirements.txt',
-            'Pipfile',
-            'pyrightconfig.json',
+-- Javascript/Eslint
+lsp.eslint.setup {
+  cmd = { "vscode-eslint-language-server", "--stdio" }
+  filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx", "vue" }
+  handlers = {
+    ["eslint/confirmESLintExecution"] = <function 1>,
+      ["eslint/noLibrary"] = <function 2>,
+        ["eslint/openDoc"] = <function 3>,
+          ["eslint/probeFailed"] = <function 4>
+          }
+          on_new_config = function(config, new_root_dir)
+            -- The "workspaceFolder" is a VSCode concept. It limits how far the
+            -- server will traverse the file system when locating the ESLint config
+            -- file (e.g., .eslintrc).
+            config.settings.workspaceFolder = {
+              uri = new_root_dir,
+              name = vim.fn.fnamemodify(new_root_dir, ':t'),
+            }
+          end,
+          root_dir = function(startpath)
+            return M.search_ancestors(startpath, matcher)
+          end
+          settings = {
+            codeAction = {
+              disableRuleComment = {
+                enable = true,
+                location = "separateLine"
+              },
+              showDocumentation = {
+                enable = true
+              }
+            },
+            codeActionOnSave = {
+              enable = false,
+              mode = "all"
+            },
+            format = true,
+            nodePath = "",
+            onIgnoredFiles = "off",
+            packageManager = "npm",
+            quiet = false,
+            rulesCustomizations = {},
+            run = "onType",
+            useESLintClass = false,
+            validate = "on",
+            workingDirectory = {
+              mode = "auto"
+            }
+          }
         }
-        return util.root_pattern(unpack(root_files))(fname) or util.find_git_ancestor(fname) or util.path.dirname(fname)
-    end,
-    settings = {
-        python = {
-            analysis = {
+
+        -- PHP
+        lsp.intelephense.setup {
+          coq.lsp_ensure_capabilities{
+            cmd = { "intelephense", "--stdio" },
+            filetypes = { "php" },
+            capabilities = capabilities,
+          }
+        }
+
+        -- Python
+        lsp.pyright.setup{
+          cmd = { "pyright-langserver", "--stdio" },
+          filetypes = { "python" },
+          root_dir = function(fname)
+            local root_files = {
+              'pyproject.toml',
+              'setup.py',
+              'setup.cfg',
+              'requirements.txt',
+              'Pipfile',
+              'pyrightconfig.json',
+            }
+            return util.root_pattern(unpack(root_files))(fname) or util.find_git_ancestor(fname) or util.path.dirname(fname)
+          end,
+          settings = {
+            python = {
+              analysis = {
                 autoSearchPaths = true,
                 diagnosticMode = "workspace",
                 useLibraryCodeForTypes = true
+              }
             }
+          },
+          -- on_attach=require'coq'.on_attach,
+          capabilities = capabilities,
         }
-    },
-    -- on_attach=require'coq'.on_attach,
-    capabilities = capabilities,
-}
