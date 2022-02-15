ba0268d7ed631b115134393176467b724112e358
Joshua Ryan V. Velasquez <joshuaryan.velasquez.0322@gmail.com>
2022-02-15 12:50:07 +0800

    refactor(lua): clean init.lua file

--- lua/init.lua
+++ lua/init.lua
@@ -1,4 +1,5 @@
-local packer = require 'packer'
+local packer = require('packer')
+local lsp = require('lspconfig')
 local use = packer.use
 
 packer.init{
@@ -9,33 +10,35 @@ packer.reset()
 
 packer.startup(function()
 
+  -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
   -- Linters & Fixers
-  -- use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
 
   -- LSP, Completions & Snippets
-  -- use {
-    -- 'neovim/nvim-lspconfig',
-    -- requires = {
-    --   {'ms-jpq/coq_nvim', branch = 'coq'}
-    -- },
-  -- }
+  use 'neovim/nvim-lspconfig'
 
-  -- use {
-    -- 'nvim-telescope/telescope.nvim',
-    -- requires = {
-    --   {'nvim-lua/plenary.nvim'},
-    --   {'nvim-lua/popup.nvim'}
-    -- }
-  -- }
+  -- Trouble
+  use {
+    "folke/trouble.nvim",
+    requires = "kyazdani42/nvim-web-devicons",
+    config = function()
+      require("trouble").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end
+  }
 
-  -- use 'lewis6991/impatient.nvim'
 end)
 
--- Load configurations
--- local init = function ()
---   require("_lsp/_all")
--- end
+-- Configs
+local init = function()
+  require('_lsp/_html')
+  require('_lsp/_eslint')
+  require('_lsp/_tsserver')
+end
 
-init() --> Load configs
+init()
