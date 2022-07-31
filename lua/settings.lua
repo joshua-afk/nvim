281fd2243e6d836d7d613fa4d9d6d874562198a6
Joshua Ryan V. Velasquez <joshuaryan.velasquez.0322@gmail.com>
2022-07-31 21:23:24 +0800

    feat(settings): migrate my complete settings to lua

---
+++ lua/settings.lua
@@ -0,0 +1,49 @@
+g = vim.g
+o = vim.o
+opt = vim.opt
+api = vim.api
+
+o.t_Co=256
+o.encoding='UTF-8'
+o.hidden=true
+o.cmdheight=0
+o.updatetime=100
+o.shortmess=o.shortmess .. "c"
+o.scrolloff=8
+o.number=true
+o.relativenumber=true
+o.cursorline=true
+o.laststatus=2
+o.linespace=4
+o.showmode=true
+o.nohlsearch=true
+o.nowrap=true
+o.tags='tags'
+o.shiftround=true
+o.backspace='indent,eol,start'
+o.autoindent=true
+o.copyindent=true
+o.ignorecase=true
+o.smartcase=true
+o.splitbelow=true
+o.splitright=true
+o.showcmd=true
+o.viewoptions = o.viewoptions:gsub('options', '')
+o.mouse='a'
+o.ttyfast=true
+o.lazyredraw=true
+o.noswapfile=true
+o.nobackupfile=true
+o.nowritebackup=true
+o.undofile=true
+o.clipboard='unnamedplus'
+o.nofixendofline=true
+o.nofixeol=true
+
+opt.undodir='~/.nvim/undodir'
+
+-- TAB
+o.tabstop=2                           					-- Width of tab character
+o.softtabstop=2                       					-- Fine tunes the amount of white space to be added
+o.shiftwidth=2                        					-- Number of spaces to use for autoindenting
+o.expandtab=true                           				-- Use spaces instead of tabs
