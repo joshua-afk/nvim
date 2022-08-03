f3e4b6182b8025f3799a73ba12e8c44a5348328b
Joshua Ryan V. Velasquez <joshuaryan.velasquez.0322@gmail.com>
2022-08-03 22:34:50 +0800

    feat(maps): migrate mappings from vimscript to lua

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,5 +5,60 @@ options = { noremap = true }
 
 g.mapleader = " "
 
-api.nvim_set_keymap('n', '<leader>w', ':w<cr>', options)
-api.nvim_set_keymap('n', '<leader>q', ':q<cr>', options)
+-- Quickly open vimrc
+api.nvim_set_keymap('n', '<leader>vrc', ':vsplit $MYVIMRC <cr>', options)
+
+-- Fast save & quit
+api.nvim_set_keymap('n', '<leader>w', ':w <cr>', options)
+api.nvim_set_keymap('n', '<leader>q', ':q <cr>', options)
+
+-- Easier split navigation (QWERTY)
+api.nvim_set_keymap('n', '<c-j>', '<c-w><c-j>', options)
+api.nvim_set_keymap('n', '<c-k>', '<c-w><c-k>', options)
+api.nvim_set_keymap('n', '<c-l>', '<c-w><c-l>', options)
+api.nvim_set_keymap('n', '<c-h>', '<c-w><c-h>', options)
+-- Easier split navigation (COLEMAK-DH)
+-- api.nvim_set_keymap('n', '<c-w><c-n>', '<c-w><c-j>', options)
+-- api.nvim_set_keymap('n', '<c-w><c-e>', '<c-w><c-k>', options)
+-- api.nvim_set_keymap('n', '<c-w><c-i>', '<c-w><c-l>', options)
+-- api.nvim_set_keymap('n', '<c-w><c-h>', '<c-w><c-h>', options)
+
+-- Up & Down wrapper reverse
+api.nvim_set_keymap('n', 'gk', 'k', options)
+api.nvim_set_keymap('n', 'gj', 'j', options)
+api.nvim_set_keymap('n', 'k', 'gk', options)
+api.nvim_set_keymap('n', 'j', 'gj', options)
+
+-- Remap beginning of the line
+api.nvim_set_keymap('n', '0', '^', options)
+api.nvim_set_keymap('n', '^', '0', options)
+
+-- Easy buffers
+api.nvim_set_keymap('n', '<leader>n', ':bn <cr>', options)
+api.nvim_set_keymap('n', '<leader>b', ':bp <cr>', options)
+
+-- Copy to system clipboard
+api.nvim_set_keymap('v', '<leader>y', '"+y', options)
+api.nvim_set_keymap('n', '<leader>Y', '"+yg_', options)
+api.nvim_set_keymap('n', '<leader>y', '"+y', options)
+api.nvim_set_keymap('n', '<leader>yy', '"+yy', options)
+
+-- Paste from system clipboard
+api.nvim_set_keymap('n', '<leader>p', '"+p', options)
+api.nvim_set_keymap('n', '<leader>P', '"+P', options)
+api.nvim_set_keymap('v', '<leader>p', '"+p', options)
+api.nvim_set_keymap('v', '<leader>P', '"+P', options)
+
+-- Hit <enter> to insert new line in normal mode
+api.nvim_set_keymap('n', '<enter>', 'o<esc>', options)
+api.nvim_set_keymap('n', 's<enter>', 'o<esc>', options)
+
+-- Close HTML tag
+api.nvim_set_keymap('i', '<c-c>', '</<c-x><c-o><c-x>', { noremap = true, silent = true })
+
+-- Faster open splits
+api.nvim_set_keymap('n', '<leader>vs', ':vsplit <cr>', options)
+api.nvim_set_keymap('n', '<leader>sp', ':split <cr>', options)
+
+-- Vertical resize for sidenav
+api.nvim_set_keymap('n', '<leader>30', ':vertixcal resize 30 <cr>', options)
