9dd97bcfa862de206b99163678fd309a466607d3
Joshua Ryan V. Velasquez <joshuaryan.velasquez.0322@gmail.com>
2022-09-05 16:32:07 +0800

    feat(plug): install vim-diff-enhance

--- lua/plug.lua
+++ lua/plug.lua
@@ -1,9 +1,11 @@
 local Plug = vim.fn['plug#']
 vim.call('plug#begin', '~/.config/nvim/plugged')
 
+-- Core
+Plug 'chrisbra/vim-diff-enhanced'
+
 -- GIT
 Plug 'tpope/vim-fugitive'
 Plug 'f-person/git-blame.nvim'
 
 vim.call('plug#end')
-
