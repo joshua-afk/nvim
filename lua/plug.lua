800b73849c3c844e5c42cb52c8efd64264c282c3
Joshua Ryan V. Velasquez <joshuaryan.velasquez.0322@gmail.com>
2022-09-05 21:15:40 +0800

    feat(plug): install navigation plugins

--- lua/plug.lua
+++ lua/plug.lua
@@ -4,8 +4,15 @@ vim.call('plug#begin', '~/.config/nvim/plugged')
 -- Core
 Plug 'chrisbra/vim-diff-enhanced'
 
+-- Navigation
+Plug('mg979/vim-visual-multi', {branch = 'master'})
+Plug 'MattesGroeger/vim-bookmarks'
+Plug 'psliwka/vim-smoothie'
+Plug 'unblevable/quick-scope'
+Plug 'airblade/vim-rooter'
+
 -- GIT
 Plug 'tpope/vim-fugitive'
-Plug 'f-person/git-blame.nvim'
+--Plug 'f-person/git-blame.nvim'
 
 vim.call('plug#end')
