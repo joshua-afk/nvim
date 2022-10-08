9d2534786350c7d325873948b41f7853a4098fd0
Joshua Ryan V. Velasquez <joshuaryan.velasquez.0322@gmail.com>
2022-10-08 21:48:57 +0800

    feat(snip): add HTML snip & extend it to blade

--- plugin/luasnip.lua
+++ plugin/luasnip.lua
@@ -1,1 +1,21 @@
+local ls = require 'luasnip'
+local types = require 'luasnip.util.types'
+
+ls.setup({
+  history = true,
+  -- Update more often, :h events for more info.
+	update_events = "TextChanged,TextChangedI",
+  enable_autosnippets = true,
+  ext_opts = {
+    [types.choiceNode] = {
+      active = {
+        virt_text = { { "choiceNode", "Comment" } },
+      },
+    },
+  },
+})
+
+-- extend html snippet to blade
+ls.filetype_extend("blade", {"html"})
+
 require("luasnip.loaders.from_snipmate").load({ paths = { "~/.config/nvim/snippets" } })
