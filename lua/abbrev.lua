3d088ca2596b36c54a574786724510c90a5858af
Joshua Ryan V. Velasquez <joshuaryan.velasquez.0322@gmail.com>
2022-11-26 16:11:02 +0800

    feat(abbrev): add Custom Telescope grep shortcut
    Telescope grep files with filter for file types

--- lua/abbrev.lua
+++ lua/abbrev.lua
@@ -2,4 +2,6 @@ vim.cmd('abb vr vertical resize')
 vim.cmd('abb vdiff vertical diffsplit')
 vim.cmd('abb pupdate execute "PackerSync" PlugUpdate')
 vim.cmd('abb exp !explorer.exe .')
-vim.cmd('abb code !code .')
+vim.cmd('abb vscode !code .')
+vim.cmd('abb tgrep lua require("telescope.builtin").live_grep { noremap = true, prompt_title="Grep with Filtered Files", additional_args=function() return {"-g*.js"} end}')
+
