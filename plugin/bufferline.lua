f503fd667d162a797c82b5afdf9ee24536261fdc
Joshua Ryan V. Velasquez <joshuaryan.velasquez.0322@gmail.com>
2022-12-11 10:40:07 +0800

    feat(config): update bufferline config

--- plugin/bufferline.lua
+++ plugin/bufferline.lua
@@ -1,6 +1,14 @@
 api = vim.api
 bufferline = require('bufferline')
 
-bufferline.setup({})
+bufferline.setup({
+    options = {
+      max_name_length = 30,
+      hover = {
+        enabled = false,
+      },
+      show_duplicate_prefix = false
+    }
+})
 
 api.nvim_set_keymap('n', '<c-t>', ':BufferLinePick<cr>', { noremap = true })
