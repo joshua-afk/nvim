c210305bef25bfc73c6609768de20d86713bbcf4
Joshua Ryan V. Velasquez <joshuaryan.velasquez.0322@gmail.com>
2023-07-17 16:31:52 +0800

    feat(abbrev): clean and update abbreviations

--- lua/abbrev.lua
+++ lua/abbrev.lua
@@ -1,7 +1,17 @@
+-- Reload luafile
+vim.cmd('abb reload exe "luafile %" <Esc>')
+
+-- Pane controls
 vim.cmd('abb vr vertical resize')
 vim.cmd('abb vdiff vertical diffsplit')
+
+-- Plugins sync and update
 vim.cmd('abb pupdate execute "PackerSync" PlugUpdate')
+
+-- Telescope commands shortcuts
+vim.cmd('abb tgrep lua require("telescope.builtin").live_grep { noremap = true, prompt_title="Grep with Filtered Files", additional_args=function() return {"-g*.js"} end}')
+
+-- Windows applications open
 vim.cmd('abb exp !explorer.exe .')
 vim.cmd('abb vscode !code .')
-vim.cmd('abb tgrep lua require("telescope.builtin").live_grep { noremap = true, prompt_title="Grep with Filtered Files", additional_args=function() return {"-g*.js"} end}')
 
